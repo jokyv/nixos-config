283a156127de1602181c2c7c755f84f90f9c57f6
John Kyvetos <johnkyvetos@gmail.com>
2025-07-27 17:27:26 +0800

    feat(git): add count parameter to log aliases
     - Added shell functions for 'l' and 'graph' with default of 5 commits
     - Format: `git l` shows 5 commits, `git l 10` shows 10 commits
     - Preserved existing formatting and color schemes
     - Maintained all other alias functionality

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -81,8 +81,8 @@
     extraConfig = {
       alias = {
         ignore = "!gi() { local IFS=','; curl -sL \"https://www.toptal.com/developers/gitignore/api/\\$*\"; }; gi";
-        l = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit -5";
-        graph = "log --all --graph --decorate --oneline -5";
+        l = "!f() { count=${1:-5}; git log -$count --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit; }; f";
+        graph = "!f() { count=${1:-5}; git log -$count --all --graph --decorate --oneline; }; f";
       };
       init = {
         defaultBranch = "main";
