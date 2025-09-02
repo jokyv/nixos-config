f7295884a64755aaa3ae8a47bd3ef6ecf4e70cd3
John Kyvetos <johnkyvetos@gmail.com>
2025-09-02 19:18:51 +0800

    refactor(home): Simplify service definitions and organize packages
    - Replace manual systemd units for swww and waybar with the corresponding home-manager modules (`services.swww` and `services.waybar.systemd`) for a cleaner and more declarative configuration.
    - Reorganize the `home.packages` list by grouping packages into categories for improved readability.
    - Enable Wayland support for Firefox.
    - Remove commented-out and unused code from `home/default.nix`.

--- home/programs/swww.nix
+++ home/programs/swww.nix
@@ -1,41 +1,24 @@
 { config, pkgs, ... }:
 
 {
-  # Install swww
-  home.packages = [ pkgs.swww ];
-
-  # Start swww on login
-  systemd.user.services.swww-init = {
-    Unit = {
-      Description = "Start swww daemon";
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      ExecStart = "${pkgs.swww}/bin/swww init";
-      Type = "oneshot";
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
-  };
-
-  # Set a wallpaper after the daemon starts
-  systemd.user.services.swww-set = {
-    Unit = {
-      Description = "Set wallpaper with swww";
-      After = [ "swww-init.service" ];
-      Requires = [ "swww-init.service" ];
-    };
-    Service = {
-      Type = "oneshot";
-      ExecStart = ''
-        ${pkgs.swww}/bin/swww img ${config.home.homeDirectory}/pics/wallpapers/gankar_1.png \
-          --transition-type=any \
-          --transition-fps=60
-      '';
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
+  services.swww = {
+    enable = true;
+    systemd.enable = true;
+    settings = {
+      "output HDMI-A-1" = {
+        image = "${config.home.homeDirectory}/pics/wallpapers/gankar_1.png";
+        transition = {
+          type = "any";
+          fps = 60;
+        };
+      };
+      "output DP-1" = {
+        image = "${config.home.homeDirectory}/pics/wallpapers/gankar_1.png";
+        transition = {
+          type = "any";
+          fps = 60;
+        };
+      };
     };
   };
 }
