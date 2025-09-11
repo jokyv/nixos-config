ecef54f7a1ab81bf4a04723d98ed883093ac5510
John Kyvetos <johnkyvetos@gmail.com>
2025-09-11 10:54:45 +0800

    feat(host): Add ZRAM swap and LUKS encryption option
    Enable zramSwap for better performance. The disk configuration is updated with a swap partition and a commented-out example for an encrypted BTRFS root partition using LUKS.

--- hosts/jokyv/disk-config.nix
+++ hosts/jokyv/disk-config.nix
@@ -21,9 +21,54 @@
                 mountpoint = "/boot";
               };
             };
+            swap = {
+              size = "16G"; # Adjust size as needed, e.g. to match RAM size for hibernation
+              content = {
+                type = "swap";
+                # Recommended for security, creates an encrypted swap space
+                randomisedEncryption = true;
+              };
+            };
+
             # Create a btrfs partition with the rest of the space
+
+            # --------------------------------------------------------------------
+            # --- Encrypted BTRFS Partition (LUKS) - UNCOMMENT TO USE ---
+            # To enable, remove the /* and */ block comments around this section,
+            # and add them around the unencrypted section below.
+            # --------------------------------------------------------------------
+            /*
             root = {
-              size = "100%";
+              size = "100%"; # disko will assign the rest of the space to this partition
+              content = {
+                type = "luks";
+                name = "crypted"; # This will be the name of the unlocked device
+                # You will be prompted for a password during installation and on every boot.
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-L" "nixos" ]; # Label the filesystem
+                  subvolumes = {
+                    # Create a subvolume for the root filesystem
+                    "/@" = {
+                      mountpoint = "/";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                    # Create a subvolume for the home directory
+                    "/@home" = {
+                      mountpoint = "/home";
+                      mountOptions = [ "compress=zstd" "noatime" ];
+                    };
+                  };
+                };
+              };
+            };
+            */
+
+            # ----------------------------------------------------------------------
+            # --- Unencrypted BTRFS Partition - COMMENT OUT IF USING LUKS ---
+            # ----------------------------------------------------------------------
+            root = {
+              size = "100%"; # disko will assign the rest of the space to this partition
               content = {
                 type = "btrfs";
                 extraArgs = [ "-L" "nixos" ]; # Label the filesystem
