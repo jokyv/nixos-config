df3a3e14c8d3714340f27e33fd06faff5b539a59
John Kyvetos <johnkyvetos@gmail.com>
2025-10-10 16:31:07 +0800

    feat: enable bluetooth and pipewire jack/32-bit alsa support

--- hosts/jokyv/services.nix
+++ hosts/jokyv/services.nix
@@ -14,7 +14,9 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
 
   # Display Management
