88276b88969b1085a73129c16139079b74d88ef4
John Kyvetos <johnkyvetos@gmail.com>
2026-01-24 21:23:28 +0800

    refactor: extract system automation to maintenance.nix
    Closes #13

--- hosts/jokyv/default.nix
+++ hosts/jokyv/default.nix
@@ -16,6 +16,8 @@ in
     ./services.nix
     # btrfs configuration
     ./btrfs.nix
+    # maintenance automation
+    ./maintenance.nix
     # import niri module
     inputs.niri.nixosModules.niri
   ];
@@ -196,81 +198,6 @@ in
     LC_PAPER = "en_SG.UTF-8";
     LC_TELEPHONE = "en_SG.UTF-8";
     LC_TIME = "en_SG.UTF-8";
-  };
-
-  # ---------------------------------------------
-  # System Automation
-  # ---------------------------------------------
-
-  # check by running 'systemctl list-timers'
-
-  nix = {
-    # auto optimisation
-    optimise = {
-      automatic = true;
-      # dates = "weekly";
-      dates = "Sun 11:00";
-      persistent = true;
-      randomizedDelaySec = "45min";
-    };
-
-    # Garbage Collection
-    gc = {
-      automatic = true;
-      # dates = "weekly";
-      dates = "Sun 11:00";
-      options = "--delete-older-than 7d";
-      persistent = true;
-      randomizedDelaySec = "45min";
-    };
-
-    # Nix Configuration
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      trusted-users = [
-        "root"
-        "jokyv"
-      ];
-      warn-dirty = false;
-
-      # Binary caches for faster builds
-      substituters = [
-        "https://cache.nixos.org/"
-        "https://nix-community.cachix.org"
-        "https://vicinae.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-      ];
-    };
-
-    # Enable nix-direnv for development environments
-    package = pkgs.nixVersions.stable;
-  };
-
-  # Automatic system updates
-  system.autoUpgrade = {
-    enable = true;
-    dates = "Sat 11:00";
-    allowReboot = false;
-    # flake = "github:jokyv/nixos-config";
-    # flake = "${userHome}/nixos-config";
-    flake = inputs.self.outPath;
-    flags = [
-      # "--update-input"
-      "--update"
-      "nixpkgs"
-      "-L"
-      "--commit-lock-file"
-    ];
-    randomizedDelaySec = "45min";
-    persistent = true;
   };
 
   # ---------------------------------------------
