ae129983997fb5dac1504b897f5024a54ad84dae
John Kyvetos <johnkyvetos@gmail.com>
2026-06-21 22:58:19 +0800

    refactor(config): remove old entrypoints

--- home/common.nix
+++
@@ -1,9 +0,0 @@
-{ config, ... }:
-
-{
-  home.username = "jokyv";
-  home.homeDirectory = "/home/${config.home.username}";
-  home.stateVersion = "24.05";
-  programs.home-manager.enable = true;
-  systemd.user.startServices = "sd-switch";
-}
